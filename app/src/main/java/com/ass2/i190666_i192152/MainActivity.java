f3342d83966488d2eb294be85fa229b58b9895f0
umaidkhakwani <i190666@nu.edu.pk>
2022-10-17 14:55:05 +0500

    start

---
+++ app/src/main/java/com/ass2/i190666_i192152/MainActivity.java
@@ -0,0 +1,14 @@
+package com.ass2.i190666_i192152;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
